3b459d26ff6b6a04039ecf7caee10d1d11058760
Hao-Xin Wang <wanghaoxin1996@gmail.com>
2023-11-11 15:01:38 +0800

    update note

---
+++ vmc_peps_note/Kagome4x4LocalUpdateAutoCorrelation.m
@@ -0,0 +1,27 @@
+data = load("energy0");
+t_list = 1:30;
+A_list = zeros(1,numel(t_list)); %auto correlation
+mu = mean(data);
+std_energ = std(data);
+for i = 1:numel(t_list)
+    t = t_list(i);
+    A_list(i) = mean(data(1:end-t) .*data(1+t:end))-mu*mu;
+end
+plot(t_list, (A_list)/std_energ,'-o'); hold on;
+
+fit_model = fit(t_list(1:5)', abs(A_list(1:5))', 'exp1');
+% semilogy(t_list(1:5), fit_model(t_list(1:5)));
+fprintf('Autocorrelation Length: %.2f\n', -1 / fit_model.b);
+
+% set(l,'Box','off');set(l,'Interpreter','latex');
+% set(l,'Fontsize',24);
+% set(l,'Location','SouthWest');
+
+
+set(gca,'fontsize',24);
+set(gca,'linewidth',1.5);
+set(get(gca,'Children'),'linewidth',2); % Set line width 1.5 pounds
+xlabel('$t$','Interpreter','latex');
+ylabel('$A(t)$','Interpreter','latex');
+set(get(gca,'XLabel'),'FontSize',24);
+set(get(gca,'YLabel'),'FontSize',24);
